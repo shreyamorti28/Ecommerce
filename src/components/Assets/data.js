f653f84e10446b39ac0b55d46728451380828f85
shreyamorti28 <1032221338@mitwpu.edu.in>
2024-09-05 19:39:27 +0530

    shop product,login and cart responsive design commit

--- src/components/Assets/data.js
+++ src/components/Assets/data.js
@@ -1,6 +1,9 @@
-import p1_img from './purple_tshirt.png'
-import p2_img from './light_green.png'
-import p3_img from './white_sportshoes.png'
+import p1_img from './printed_tshirt.jpg'
+import p1_img_hover from './hover_shirt.jpg'
+import p2_img from './baggy_jeans.png'
+import p2_img_hover from './baggy_jeans2.png'
+import p3_img from './hover_shoes1.jpg'
+import p3_img_hover from './hover_shoes2.jpg'
 
 
 let data_product = [
@@ -8,6 +11,8 @@ let data_product = [
         id:1,
         name:"Colourful Summer t-shirt",
         image:p1_img,
+        defaultImage: p1_img,
+        hoverImage: p1_img_hover,
         new_price:300,
         old_price:450,
     },
@@ -15,6 +20,8 @@ let data_product = [
         id:2,
         name:"Light Green Jeans",
         image:p2_img,
+        defaultImage: p2_img,
+        hoverImage: p2_img_hover,
         new_price:700,
         old_price:850,
     },
@@ -22,6 +29,8 @@ let data_product = [
         id:3,
         name:"White Sport Shoes",
         image:p3_img,
+        defaultImage: p3_img,
+        hoverImage: p3_img_hover,
         new_price:500,
         old_price:650,
     },
--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -1,21 +1,47 @@
-import React from 'react'
-import './Popular.css'
-import data_product from '../Assets/data'
-import Item from '../Item/Item'
+import React, { useState } from 'react';
+import './Popular.css';
+import data_product from '../Assets/data';
 
 const Popular = () => {
+  const [hoveredItemId, setHoveredItemId] = useState(null);
+
+  const handleMouseEnter = (id) => {
+    setHoveredItemId(id);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredItemId(null);
+  };
+
   return (
     <div className='popular'>
       <h1>Summer Collection</h1>
       <hr />
-      <div className="popular-item" >
-        {data_product.map((item,i)=>{
-          return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-        })}
+      <div className="popular-item">
+        {data_product.map((item) => (
+          <div
+            key={item.id}
+            className="item"
+            onMouseEnter={() => handleMouseEnter(item.id)}
+            onMouseLeave={handleMouseLeave}
+          >
+            <div className="image-container">
+            <img
+                  src={`${hoveredItemId === item.id ? item.hoverImage : item.image}?t=${new Date().getTime()}`}
+                  alt={item.name}
+                  className="item-image"
+            />
+            </div>
+            <h2>{item.name}</h2>
+            <p className="price">
+              <span className="new-price">${item.new_price}</span>
+              <span className="old-price">${item.old_price}</span>
+            </p>
+          </div>
+        ))}
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Popular
+export default Popular;
