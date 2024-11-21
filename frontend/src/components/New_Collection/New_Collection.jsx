18ee8f841408995d2347a4544faaf18d79fccfbb
shreyamorti_28 <1032221338@mitwpu.edu.in>
2024-11-21 23:08:41 +0530

    all other  pages modifying commit

--- frontend/src/components/New_Collection/New_Collection.jsx
+++ frontend/src/components/New_Collection/New_Collection.jsx
@@ -1,35 +1,35 @@
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Item from '../Item/Item';
-import './New_Collection.css'; // Updated CSS import
+import './New_Collection.css';
 
 const NewCollection = () => {
-  const [new_collection,setNew_collection]=useState([]);
+  const [new_collection, setNew_collection] = useState([]);
 
-  const items = [...new_collection, ...new_collection];
-  useEffect(()=>{
+  useEffect(() => {
     fetch('http://localhost:4000/newcollections')
-    .then((response)=>response.json())
-    .then((data)=>setNew_collection(data));
-  },[])
+      .then((response) => response.json())
+      .then((data) => setNew_collection(data));
+  }, []);
+
+  // Duplicate items for seamless looping
+  const items = [...new_collection, ...new_collection];
 
   return (
-    <div className='new_collections'>
+    <div className="new_collections">
       <h1>New Collections</h1>
-      <div className='slider'>
-        <div className='slider-wrapper'>
-          <div className='slider-content'>
-            {items.map((item, i) => (
-              <div className='new_collection-item' key={i}>
-                <Item 
-                  id={item.id} 
-                  name={item.name} 
-                  image={item.image} 
-                  new_price={item.new_price} 
-                  old_price={item.old_price} 
-                />
-              </div>
-            ))}
-          </div>
+      <div className="slider">
+        <div className="slider-wrapper">
+          {items.map((item, i) => (
+            <div className="new_collection-item" key={i}>
+              <Item
+                id={item.id}
+                name={item.name}
+                image={item.image}
+                new_price={item.new_price}
+                old_price={item.old_price}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
