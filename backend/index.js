5aa648514ad77c24702d4f6a6cf4c71db795ebcd
shreyamorti_28 <1032221338@mitwpu.edu.in>
2024-11-28 17:01:52 +0530

    Revert "Update index.js"
    This reverts commit 230a8ec0ee0ac31739dd40754639822dac57b807.

--- backend/index.js
+++ backend/index.js
@@ -55,7 +55,7 @@ app.use('/images', express.static(path.join(__dirname, 'upload/images')));
 app.post("/upload", upload.single('product'), (req, res) => {
     res.json({
         success: 1,
-        image_url: `https://ecommerce-tc7k.onrender.com/images/${req.file.filename}`
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
     });
 });
 
